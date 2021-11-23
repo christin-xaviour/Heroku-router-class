7a67ef00735c35d07a13b980ad3bf57a049bf6b4
Christin Xaviour <christinxaviour@gmail.com>
2021-11-23 10:25:21 +0530

    Router class Deployment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { GradesComponent } from './grades/grades.component';
+import { DatesheetComponent } from './datesheet/datesheet.component';
+import { SeatingComponent } from './seating/seating.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GradesComponent,
+    DatesheetComponent,
+    SeatingComponent,
+    ResultComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'App',
+      component: AppComponent},
+      {path: 'grades',
+      component: GradesComponent},
+      {path: 'datesheet',
+      component: DatesheetComponent},
+      {path: 'result',
+      component: ResultComponent},
+      {path: 'seating',
+      component: SeatingComponent}
+      ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/datesheet/datesheet.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-datesheet',
+  templateUrl: './datesheet.component.html',
+  styleUrls: ['./datesheet.component.css']
+})
+export class DatesheetComponent implements OnInit {
+
+  // datesheet= [
+  //   {'course':'CAP917','date':'12 Nov'},
+  //   {'course':'CAP918','date':'13 Nov'},
+  //   {'course':'CAP919','date':'14 Nov'}
+  // ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
